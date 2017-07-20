5cb6551f8a22fb624e21b8e7dc953112c7516291
ydh <ydh1304@naver.com>
2017-07-20 18:09:51 +0900

    update

--- app/src/main/java/com/example/uniquitousprototype/ApiService.java
+++ app/src/main/java/com/example/uniquitousprototype/ApiService.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 /**
@@ -25,5 +27,10 @@ public interface ApiService {
     @POST("bills/")
     Call<Task> postNewTask(@Body Task task);
             //@Header("Authorization") String token, @Body Task task);
+    @DELETE("bills/{id}/")
+    Call<Void> deleteTask(@Header("Authorization") String token, @Path("id") int id);
+
+    @PUT("bills/{id}/")
+    Call<Task> updateTask(@Header("Authorization")String token, @Path("id")int id, @Body Task task);
 
 }
--- app/src/main/java/com/example/uniquitousprototype/CreateTaskIntent.java
+++ app/src/main/java/com/example/uniquitousprototype/CreateTaskIntent.java
@@ -21,6 +21,12 @@ public class CreateTaskIntent extends AppCompatActivity{
     RadioGroup categoryGroup;
     private ApiApplication apiApplication;
     private ApiService apiService;
+    String content;
+    String category;
+    int cost;
+    int reward;
+    int type;
+    int id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,11 +34,38 @@ public class CreateTaskIntent extends AppCompatActivity{
 
         apiApplication = (ApiApplication) getApplicationContext();
         apiService = apiApplication.getApiService();
-
+        Bundle extras = getIntent().getExtras();
         contentEditText = (EditText)findViewById(R.id.text);
         costEditText = (EditText)findViewById(R.id.cost1);
         rewardEditText = (EditText)findViewById(R.id.cost2);
         categoryGroup = (RadioGroup)findViewById(R.id.radioGroup1);
+        content = extras.getString("content");
+        category = extras.getString("category");
+        cost = extras.getInt("cost");
+        reward = extras.getInt("reward");
+        type = extras.getInt("type");
+        id = extras.getInt("id");
+        if(type==1)
+        {
+            contentEditText.setText(content);
+            costEditText.setText(String.valueOf(cost));
+            rewardEditText.setText(String.valueOf(reward));
+            switch(category)
+            {
+                case "DELEVERY":
+                    categoryGroup.check(R.id.radio0);
+                    break;
+                case "HOMEWORK":
+                    categoryGroup.check(R.id.radio1);
+                    break;
+                case "ERRAND":
+                    categoryGroup.check(R.id.radio2);
+                    break;
+                case "ETC":
+                    categoryGroup.check(R.id.radio3);
+                    break;
+            }
+        }
     }
 
     public void create(View view){
@@ -45,18 +78,35 @@ public class CreateTaskIntent extends AppCompatActivity{
 
         Task newTask = new Task(content, category, cost, reward);
         String token = "Token 036db40131c8e0bf24f2b70d74642b5170f592a6";
-        Call<Task> call = apiService.postNewTask(newTask);
-        call.enqueue(new Callback<Task>() {
-            @Override
-            public void onResponse(Call<Task> call, Response<Task> response) {
+        if(type ==0) {
+            Call<Task> call = apiService.postNewTask(newTask);
+            call.enqueue(new Callback<Task>() {
+                @Override
+                public void onResponse(Call<Task> call, Response<Task> response) {
 
-            }
+                }
 
-            @Override
-            public void onFailure(Call<Task> call, Throwable t) {
+                @Override
+                public void onFailure(Call<Task> call, Throwable t) {
 
-            }
-        });
+                }
+            });
+        }
+        else{
+            Call<Task> call = apiService.updateTask(token,id,newTask);
+            call.enqueue(new Callback<Task>() {
+                @Override
+                public void onResponse(Call<Task> call, Response<Task> response) {
+
+                }
+
+                @Override
+                public void onFailure(Call<Task> call, Throwable t) {
+
+                }
+            });
+
+        }
 
         finish();
     }
