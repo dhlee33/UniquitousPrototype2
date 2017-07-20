b5948c2996074713a8d28ad7bd747a9610b1823b
yks1502 <516yks@naver.com>
2017-07-20 16:14:25 +0900

    asdf

--- app/src/main/java/com/example/uniquitousprototype/ApiService.java
+++ app/src/main/java/com/example/uniquitousprototype/ApiService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
