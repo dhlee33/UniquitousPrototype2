1399f625cb9d2585716eb8a333b77c0f9d063a99
yks1502 <516yks@naver.com>
2017-08-04 13:39:30 +0900

    pullrequest test

--- app/src/main/java/com/example/uniquitousprototype/MainActivity.java
+++ app/src/main/java/com/example/uniquitousprototype/MainActivity.java
@@ -70,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<TaskResponse> call, Throwable t) {
                 Toast.makeText(MainActivity.this, "??", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "??", Toast.LENGTH_SHORT).show();
 
             }
         });
